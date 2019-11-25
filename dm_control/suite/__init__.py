74fdf29e996431000a148e6c38d448796d8340e2
Oleh Rybkin <oleh.rybkin@gmail.com>
2019-11-25 14:57:42 -0500

    add dynamic walker and cheetah

--- dm_control/suite/__init__.py
+++ dm_control/suite/__init__.py
@@ -43,6 +43,8 @@ from dm_control.suite import reacher
 from dm_control.suite import stacker
 from dm_control.suite import swimmer
 from dm_control.suite import walker
+from dm_control.suite import dyn_walker
+from dm_control.suite import dyn_cheetah
 
 # Find all domains imported.
 _DOMAINS = {name: module for name, module in locals().items()
